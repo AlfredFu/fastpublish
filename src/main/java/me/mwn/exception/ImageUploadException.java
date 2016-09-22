f63eaea222535e11aa0c8fe71a185728c564eccf
Fred Fu <fred.fu@lexisnexis.com>
2016-09-22 17:35:12 +0800

    ..

---
+++ src/main/java/me/mwn/exception/ImageUploadException.java
@@ -0,0 +1,13 @@
+package me.mwn.exception;
+
+public class ImageUploadException  extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ImageUploadException(String msg){
+		super(msg);
+	}
+}
