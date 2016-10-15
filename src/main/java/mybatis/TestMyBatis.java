5ddf1a8b28bdf8653a5b9559604545ae1a4d17cc
Fred Fu <fred.fu@lexisnexis.com>
2016-10-15 16:25:57 +0800

    extract mvc configuration

--- src/main/java/mybatis/TestMyBatis.java
+++ src/main/java/mybatis/TestMyBatis.java
@@ -19,6 +19,8 @@ public class TestMyBatis {
 			MybatisProductDao mpd = ss.getMapper(MybatisProductDao.class);
 			Product p = mpd.getProductById(29);
 			System.out.println(p.getName() + ",  " +p.getDescription());
+			
+			ss.close();
 		}
 		
 		public static void main(String[] args){
